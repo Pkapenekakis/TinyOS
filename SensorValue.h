0920e4915286940a11b0896bf6f4aeb3184f3ebe
Pkapenekakis <56314957+Pkapenekakis@users.noreply.github.com>
2024-11-25 20:31:48 +0200

    Update SensorValue.h

--- SensorValue.h
+++ SensorValue.h
@@ -1,31 +1,14 @@
-#ifndef SIMPLEROUTINGTREE_H
-#define SIMPLEROUTINGTREE_H
-
+#ifndef SENSORVALUE_H
+#define SENSORVALUE_H
 
 enum{
-	SENDER_QUEUE_SIZE=5,
-	RECEIVER_QUEUE_SIZE=3,
-	AM_SIMPLEROUTINGTREEMSG=22,
-	AM_ROUTINGMSG=22,
-	AM_NOTIFYPARENTMSG=12,
-	SEND_CHECK_MILLIS=70000,
-	TIMER_PERIOD_MILLI=150000,
-	TIMER_FAST_PERIOD=200,
-	TIMER_LEDS_MILLI=1000,
+	AM_SENSORVAL = 240,
 };
-/*uint16_t AM_ROUTINGMSG=AM_SIMPLEROUTINGTREEMSG;
-uint16_t AM_NOTIFYPARENTMSG=AM_SIMPLEROUTINGTREEMSG;
-*/
-typedef nx_struct RoutingMsg
-{
-	nx_uint16_t senderID;
-	nx_uint8_t depth;
-} RoutingMsg;
 
-typedef nx_struct NotifyParentMsg
+typedef nx_struct sensor_value
 {
-	nx_uint16_t senderID;
-	nx_uint16_t parentID;
-	nx_uint8_t depth;
-} NotifyParentMsg;
+	nx_uint16_t sensorValue;
+	nx_uint8_t count;
+} sensor_value_t;
+
 #endif
